8e441c9f01be2b23e0f1c2b037e6fa9ab0310640
poseitrader <posei.dawn@outlook.com>
2025-07-01 17:56:22 +0800

    feat: initial project setup

---
+++ sites/4Q70po5Mm3OWDBQUeyMzsR/chunk-IBXLZ3AH.mjs
@@ -0,0 +1,33 @@
+import{a as t}from"./chunk-WTJ5PBRZ.mjs";function a(n,e){return{breakpoints:[{hash:"torpu9",mediaQuery:"(min-width: 1300px)"},{hash:"m8p21b",mediaQuery:"(min-width: 1024px) and (max-width: 1299px)"},{hash:"1s9c9g7",mediaQuery:"(min-width: 440px) and (max-width: 1023px)"},{hash:"2444vp",mediaQuery:"(max-width: 439px)"}],customHTMLHeadEnd:`<!-- Amplitude Analytics Script -->
+<script src="https://cdn.amplitude.com/libs/analytics-browser-2.10.0-min.js.gz"><\/script>
+<script>
+  window.amplitude.init("3c368da9969d259557b9201028df6127", {
+      autocapture: { elementInteractions: true, pageViews: true, sessions: true }
+  });
+<\/script>
+
+<!-- Google tag (gtag.js) -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-FY7WMHDS49"><\/script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FY7WMHDS49');
+<\/script>
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1137347597326141');
+fbq('track', 'PageView');
+<\/script>
+<noscript></noscript>`,description:t(n,e).description,elements:{BOz64z3vz:"hero",CkMUn9p_g:"intro-3",cQocWcnEg:"intro-2",kxVuIqS_9:"intro",SwrrYTWYy:"navigation",WTeqxoVZX:"intro-1"},robots:"max-image-preview:large",serializationId:"framer-PEjye",socialImage:"https://framerusercontent.com/assets/BdplQtZSqmFh6GjgGxJyWbUVIcI.jpg",title:t(n,e).title||"Home",viewport:"width=device-width"}}var c=1,p={exports:{metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},default:{type:"function",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{a,c as b,p as c};
+//# sourceMappingURL=chunk-IBXLZ3AH.mjs.map
---
+++ sites/4Q70po5Mm3OWDBQUeyMzsR/chunk-WTJ5PBRZ.mjs
@@ -0,0 +1,33 @@
+function t(e,n){return{customHTMLHeadEnd:`<!-- Amplitude Analytics Script -->
+<script src="https://cdn.amplitude.com/libs/analytics-browser-2.10.0-min.js.gz"><\/script>
+<script>
+  window.amplitude.init("3c368da9969d259557b9201028df6127", {
+      autocapture: { elementInteractions: true, pageViews: true, sessions: true }
+  });
+<\/script>
+
+<!-- Google tag (gtag.js) -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-FY7WMHDS49"><\/script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FY7WMHDS49');
+<\/script>
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1137347597326141');
+fbq('track', 'PageView');
+<\/script>
+<noscript></noscript>`,description:"Tapflow lets tech professionals create and sell digital guides, workflows, and courses in minutes\u2014package hard-won knowledge, ship, get paid.",favicon:"https://framerusercontent.com/assets/WGcSfufeSAF5YUmMIQDthxsT8.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/SXxNS58LzxoYTTCyVV0dIDvGPw.png",title:"Tapflow"}}export{t as a};
+//# sourceMappingURL=chunk-WTJ5PBRZ.mjs.map
